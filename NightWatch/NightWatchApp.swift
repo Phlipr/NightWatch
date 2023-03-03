aab623856fd98987c8d1620ecedb0456a7ccfe7b
Phillip Reynolds <preynolds@atp.com>
2023-03-03 11:21:16 -0600

    Initial Commit

---
+++ NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Phillip Reynolds on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
